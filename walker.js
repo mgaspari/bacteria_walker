1283b569e4d99cbcefbac0568591f34c1e6486f4
mgaspari <marcoanthonygaspari@gmail.com>
2017-09-28 20:00:29 -0400

    Changed the function

--- walker.js
+++ walker.js
@@ -1,3 +1,10 @@
+let counter = 0
+let nums = [0,1]
+let lineX = []
+let lineY = []
+document.addEventListener('DOMContentLoaded', function(){
+  // $('body').addEventListener()
+})
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   walker = new Walker();
@@ -16,20 +23,67 @@ function Walker (){
 
 	this.display = function(){
 		stroke(0);
-		strokeWeight(15);
+		strokeWeight(10);
 		point(this.x,this.y);
 	}
 
-	this.step = function(){
-		var choice = Math.floor(Math.random() * (4 - 0)) + 0;
-		if (choice == 0){
-			this.x++;
-		} else if (choice ==1){
-				this.x--;
-		}
-			else if(choice ==2){
-				this.y++;
-		}
-		else {this.y--;}
-	}
+  var  mapItOut = () => {
+    lineX.push(this.x)
+    lineY.push(this.y)
+    lineX.forEach((val, i, arr)=>{
+      stroke(100)
+      line(lineX[i], lineY[i], lineX[i-1], lineY[i-1])
+    })
+  }
+
+  this.step = function(){
+    let nextNum = nums[nums.length - 2] + nums[nums.length - 1];
+    nums.push(nextNum);
+    let testInt = counter % 4
+    switch (testInt){
+      case 0:
+      this.x += nextNum
+      this.y -= nextNum
+      mapItOut()
+      counter++
+      break
+      case 1:
+      this.x += nextNum
+      this.y += nextNum
+      mapItOut()
+      counter++
+      break
+      case 2:
+      this.x -= nextNum
+      this.y += nextNum
+      mapItOut()
+      counter++
+      break
+      case 3:
+      this.x -= nextNum
+      this.y -= nextNum
+      mapItOut()
+      counter++
+      break
+      default:
+      "Done"
+    }
+
+
+  }
 }
+// 	this.step  = function(){
+//
+//     var disToMove = 12;
+// 		var choice = Math.floor(Math.random() * (4 - 0)) + 0;
+// 		if (choice == 0){
+// 			this.x += disToMove;
+// 		} else if (choice ==1){
+// 				this.x -= disToMove;
+// 		}
+// 			else if(choice ==2){
+// 				this.y += disToMove;
+// 		}
+// 		else {this.y -= disToMove;}
+// 	}
+// }
